79afcba5faaf758ea3caee368f9b2e81f29455bd
fb0801 <fbhatti0801@gmail.com>
2026-04-07 17:18:42 +0100

    project zenml lib project 1.3

---
+++ zen_tut_4/steps/ingest_data.py
@@ -0,0 +1,16 @@
+import logging
+
+import pandas as pd
+from zenml import step
+
+class IngestData:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    def get_data(self):
+        logging.info(f"Reading data from {self.data_path}")
+        data = pd.read_csv(self.data_path)
+        return pd.read_csv(self.data_path)
+    
+@step
+def ingest_data_step(data_path: str) -> pd.DataFrame:
