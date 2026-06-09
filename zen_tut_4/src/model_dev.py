4dd9161db59a0ac724a4bfc324820152fcf4457f
fb0801 <fbhatti0801@gmail.com>
2026-06-09 17:19:15 +0100

    working on model dev 1.1

--- zen_tut_4/src/model_dev.py
+++ zen_tut_4/src/model_dev.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC, abstractmethod 
+from sklearn.linear_model import LinearRegression
 
 class Model(ABC):
 
@@ -14,3 +15,26 @@ class Model(ABC):
         None
         """
         pass
+
+class LinearRegressionModel(Model):
+
+    """
+    linear regression model
+    """
+    def train(self, x_train, y_train, **kwargs):
+        """
+        trains the model
+        Args:
+            x_train: training data
+            y_train: training labels
+        Returns:
+            None
+        """
+        try:
+            reg = LinearRegression(**kwargs)
+            reg.fit(x_train, y_train)
+            logging.info("Model training completed")
+            return reg
+        except Exception as e:
+            logging.error("Error in training the model: {}".format(e))
+            raise e 
