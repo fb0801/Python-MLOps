03df122f0a90ab401558e9de88a5d150aa31accd
fb0801 <fbhatti0801@gmail.com>
2026-06-10 17:22:06 +0100

    working on model dev 1.2

--- zen_tut_4/steps/model_train.py
+++ zen_tut_4/steps/model_train.py
@@ -3,6 +3,20 @@ import pandas as pd
 from zenml import step
 
 
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+
 @step
-def train_model(df: pd.DataFrame) -> None:
-    pass
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame,
+
+) -> RegressorMixin:
+    """
+    trains the model on the ingested data
+    Args:
+        df: ingested data
+    """
+    model = None
