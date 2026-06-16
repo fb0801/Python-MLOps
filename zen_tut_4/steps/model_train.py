7c1bd9c667cf510c9ffe643fd366cf13b5743130
fb0801 <fbhatti0801@gmail.com>
2026-06-16 17:03:22 +0100

    working on model dev 1.3

--- zen_tut_4/steps/model_train.py
+++ zen_tut_4/steps/model_train.py
@@ -5,6 +5,7 @@ from zenml import step
 
 from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 @step
 def train_model(
@@ -12,6 +13,7 @@ def train_model(
     X_test: pd.DataFrame,
     y_train: pd.DataFrame,
     y_test: pd.DataFrame,
+    config: ModelNameConfig,
 
 ) -> RegressorMixin:
     """
@@ -19,4 +21,15 @@ def train_model(
     Args:
         df: ingested data
     """
-    model = None
+    try:
+        model = None
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = model.train(X_train, y_train)
+            return trained_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("Error in training the model: {}".format(e))
+        raise e
+
