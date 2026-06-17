2d4fb7f55602fdf9f7f286e67d9e36f3ce281e8d
fb0801 <fbhatti0801@gmail.com>
2026-06-17 16:55:54 +0100

    working on eval model 1.2

--- zen_tut_4/src/evaluation.py
+++ zen_tut_4/src/evaluation.py
@@ -1,5 +1,7 @@
 import logging
 from abc import ABC, abstractmethod
+
+from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 
 class Evaluation(ABC):
@@ -17,3 +19,17 @@ class Evaluation(ABC):
             None
         """
         pass
+
+class MSE(Evaluation):
+    """
+    evaluation strategy for calculating mean squared error
+    """
+    def calculate_scores(self, y_true: np.ndarray,y_pred: np.darray):
+        try:
+            logging.info("Calculating mean squared error")
+            mse = mean_squared_error(y_true, y_pred)
+            logging.info("MSE: {}".format(mse))
+        except Exception as e:
+            logging.error("Error in calculating MSE: {}".format(e))
+            raise e
+        
