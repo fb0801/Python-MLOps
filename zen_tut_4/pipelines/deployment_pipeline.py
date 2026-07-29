cd7b380221d90b8280a110b03447858f865804a2
fb0801 <fbhatti0801@gmail.com>
2026-07-29 18:12:10 +0100

    deploy pipeline 1.2

--- zen_tut_4/pipelines/deployment_pipeline.py
+++ zen_tut_4/pipelines/deployment_pipeline.py
@@ -1,2 +1,18 @@
 import numpy as np 
-import 
+import pandas as pd
+from materializer.custom_materializer import cs_materializer
+from zenml import pipeline, step
+from zenml.config import DockerSettings
+from zenml.constants import DEDAULT_SERVICE_START_STOP_TIMEOUT
+from zenml.integrations.constants import MLFLOW
+from zenml.intregrations.mlflow.model_deployers.mlflow_model_deployer import (
+    MLFlowModelDeployer,
+)
+from zenml.integrations.mlflow.services import MLFlowDeploymentService
+from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
+from zenml.steps import BaseParameters, Output
+
+from steps.clean_data import clean_data
+from steps.evaluation import evaluation 
+from steps.ingest_data import ingest_data
+from steps.model_train import train_model
