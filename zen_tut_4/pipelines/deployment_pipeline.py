bacd9618743530a5196e900527bdf5215c90cb89
fb0801 <fbhatti0801@gmail.com>
2026-08-04 17:34:59 +0100

    deploy pipeline 1.4

--- zen_tut_4/pipelines/deployment_pipeline.py
+++ zen_tut_4/pipelines/deployment_pipeline.py
@@ -16,3 +16,32 @@ from steps.clean_data import clean_df
 from steps.evaluation import evaluate_model
 from steps.ingest_data import ingest_df
 from steps.model_train import train_model
+
+docker_settings = DockerSettings(required_integrations=[MLFLOW])
+
+
+class DeploymentPipelineParameters(BaseParameters):
+    min_accuracy: float = 0.92
+    workers: int = 1
+    timeout: int = DEDAULT_SERVICE_START_STOP_TIMEOUT
+
+@step 
+def deployment_trigger(
+    accuracy: float, 
+    config: DeploymentTriggerConfig,
+):
+    "trigger the deployment if the accuracy is greater"
+    return accuracy >= config.min_accuracy
+
+
+@pipeline(enable_cache=False, settings={"docker": docker_settings})
+def continuous_deployment_pipeline(
+    min_accuracy: float = 0.92,
+    workers: int = 1,
+    timeout: int = DEDAULT_SERVICE_START_STOP_TIMEOUT,
+):
+    df = ingest_df()
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    r2_score, rmse = evaluate_model(model, X_test, y_test)
+    deployment_decision = deployment_trigger(rmse)
